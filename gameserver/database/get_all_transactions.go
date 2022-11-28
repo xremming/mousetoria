50dceaf92134eb73844666d2b71a2cc21c7b5652
Maximilian Remming <maximilian@remming.fi>
2022-11-28 18:45:04 +0200

    Update name

--- gameserver/database/get_all_transactions.go
+++ gameserver/database/get_all_transactions.go
@@ -2,7 +2,7 @@ package database
 
 import "gameserver/proto"
 
-// const selectTransactionsSQL = `SELECT "transactionID", "timestamp", "comment" FROM "ledger" AS l NATURAL JOIN "transaction" AS tx`
+// const selectAllTransactionsSQL = `SELECT l."transactionID", "timestamp", "comment" FROM "ledger" AS l NATURAL JOIN "transaction" AS tx`
 
 func (db Database) GetAllTransactions(req *proto.GetAllTransactionsRequest, out proto.Database_GetAllTransactionsServer) error {
 	return nil
